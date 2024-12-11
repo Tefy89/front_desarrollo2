57e626ac4707e0d85cd7b07c62e5fe2292a47237
Tefy89 <eds.ordonez@yavirac.edu.ec>
2024-12-10 20:10:21 -0500

    clase16-parte2

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,23 +3,28 @@ import { RouterModule, Routes } from '@angular/router';
 import { PerfilComponent } from './components/perfil/perfil.component';
 import { CategoriaComponent } from './inventario/components/categoria/categoria.component';
 import { AppLayoutComponent } from './layout/app.layout.component';
+import { ProductoComponent } from './inventario/components/producto/producto.component';
 
 const routes: Routes = [
   {
-    path:"",
-    component:AppLayoutComponent,
-    children:[
+    path: "",
+    component: AppLayoutComponent,
+    children: [
       {
-        path:"perfil",
-        component:PerfilComponent
+        path: "perfil",
+        component: PerfilComponent
       },
       {
-        path:"categoria",
-        component:CategoriaComponent
+        path: "categoria",
+        component: CategoriaComponent
+      },
+      {
+        path: "producto",
+        component: ProductoComponent
       }
     ]
   }
-  
+
 ];
 
 @NgModule({
