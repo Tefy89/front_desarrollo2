6a5046ff70eb5f95132987a7bf710252b0c58179
Tefy89 <eds.ordonez@yavirac.edu.ec>
2024-12-10 19:08:43 -0500

    clase16-26

--- src/app/admin/inventario/components/categoria/categoria.component.ts
+++ src/app/admin/inventario/components/categoria/categoria.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CategoriaService } from '../../services/categoria.service';
 import { FormControl, FormGroup } from '@angular/forms';
-
+import Swal from 'sweetalert2'
 interface Categoria {
   id: number,
   nombre: string,
@@ -46,20 +46,24 @@ export class CategoriaComponent {
           this.dialog_visible = false;
           this.getCategorias();
           this.categoria_id = -1;
+          this.alerta("ACTUALIZADO", "La categoria se modifico con exito", "success")
         },
         (error: any) => {
-          console.log(error);
+          this.alerta("ERROR AL ACTUALIZAR", "Verifica los datos!", "error")
         }
-      )
+      );
+      this.categoriaForm.reset();
     }
     else {
       this.categoriaService.funGuardar(this.categoriaForm.value).subscribe(
         (res: any) => {
           this.dialog_visible = false;
           this.getCategorias();
+
+          this.alerta("REGISTRADO", "La categoria se creo con exito", "success")
         },
         (error: any) => {
-          console.log(error);
+          this.alerta("ERROR AL REGISTRAR", "Verifica los datos!", "error")
         }
       )
     }
@@ -73,6 +77,36 @@ export class CategoriaComponent {
     this.categoriaForm.setValue({ nombre: cat.nombre, detalle: cat.detalle })
   }
   eliminarCategoria(cat: Categoria) {
-
+    Swal.fire({
+      title: "¿Está seguro de eliminar la categoría?",
+      text: "Una vez eliminado no se podrá recuperar!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, eliminar!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.categoriaService.funEliminar(cat.id).subscribe(
+          (res: any) => {
+            this.alerta("ELIMINADO!", "Categoria eliminada.", "success")
+            this.getCategorias();
+            this.categoria_id = -1
+          },
+          (error: any) => {
+            this.alerta("ERROR!", "Error al intentar eliminar.", "error")
+          }
+        )
+      }
+    })
   }
+
+  alerta(title: string, text: string, icon: 'success' | 'error' | 'info' | 'question') {
+    Swal.fire({
+      //title: title,
+      //text: text,
+      //icon: icon
+    })
+  }
+
 }
